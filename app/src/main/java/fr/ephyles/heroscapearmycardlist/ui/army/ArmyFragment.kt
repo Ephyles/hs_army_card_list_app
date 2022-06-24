c7b8b450c80631e41cca4c0a999bdafec1cc8cd0
Mateo ALLEGRE <Mateo900163@gmail.com>
2022-06-25 00:33:17 +0200

    Added placeholders for army Fixed padding issue on both fragments

--- app/src/main/java/fr/ephyles/heroscapearmycardlist/ui/army/ArmyFragment.kt
+++ app/src/main/java/fr/ephyles/heroscapearmycardlist/ui/army/ArmyFragment.kt
@@ -6,9 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import fr.ephyles.heroscapearmycardlist.CardDatabase
 import fr.ephyles.heroscapearmycardlist.CardDatabase.Companion.cardList
-import fr.ephyles.heroscapearmycardlist.CardModel
 import fr.ephyles.heroscapearmycardlist.R
 import fr.ephyles.heroscapearmycardlist.adapter.CardAdapter
 
