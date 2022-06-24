510562f506c59b262d51b3642938455a787766d9
Mateo ALLEGRE <Mateo900163@gmail.com>
2022-06-25 01:04:58 +0200

    Points and spaces count work but don't update while in Army tab

--- app/src/main/java/fr/ephyles/heroscapearmycardlist/ui/army/ArmyFragment.kt
+++ app/src/main/java/fr/ephyles/heroscapearmycardlist/ui/army/ArmyFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import fr.ephyles.heroscapearmycardlist.CardDatabase.Companion.cardList
@@ -19,9 +20,22 @@ class ArmyFragment : Fragment() {
     ): View {
         val view = inflater?.inflate(R.layout.fragment_army, container, false)
 
+        val armyList = cardList.filter { it.armyCount > 0 }
+
+        var points = 0
+        var spaces = 0
+
+        armyList.forEach {
+            points += it.points * it.armyCount
+            spaces += it.figures * it.spaces * it.armyCount
+        }
+
+        view.findViewById<TextView>(R.id.army_points).text = getString(R.string.army_points, points)
+        view.findViewById<TextView>(R.id.army_spaces).text = getString(R.string.army_spaces, spaces)
+
         //recup recyclerview
         val armyRecyclerView = view.findViewById<RecyclerView>(R.id.army_recycler_list)
-        armyRecyclerView.adapter = CardAdapter(context, cardList.filter { it.armyCount > 0 }, R.layout.item_card)
+        armyRecyclerView.adapter = CardAdapter(context, armyList, R.layout.item_card)
 
         return view
     }
